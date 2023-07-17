ceb9322b8aa17eab8501e397460028a7f6e0a3c2
Nata <melvia.nr@gmail.com>
2023-07-17 21:34:48 +0500

    feat: add input

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IProduct} from '../../../shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
@@ -7,16 +7,20 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    @Input() product: IProduct | null = null;
 
-    onProductBuy(event: Event) {
+    @Output() buyButtonClick = new EventEmitter<void>();
+
+    onProductBuy(event: Event): void {
         event.stopPropagation();
 
         // eslint-disable-next-line no-console
         console.log('Buy product');
+
+        this.buyButtonClick.emit();
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        return (this.product?.rating ?? 0) >= starIndex;
     }
 }
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {productsMock} from '../../shared/products/products.mock';
+import {IProduct} from '../../shared/products/product.interface';
 
 @Component({
     selector: 'app-products-list',
@@ -11,4 +13,11 @@ export class ProductsListComponent {
         // eslint-disable-next-line no-console
         console.log('Card click');
     }
+
+    onCardBuyButtonClick(product: IProduct) {
+        // eslint-disable-next-line no-console
+        console.log('Товар:', product?.name, 'Цена:', product?.price, 'рейтинг:', product?.rating);
+    }
+
+    productList = productsMock;
 }
